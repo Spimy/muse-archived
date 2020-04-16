7dd4118a1887ca2a30bd8638f0fa2534df3c1abf
unknown <officialspyme@gmail.com>
2020-04-16 21:18:03 +0400

    Add events listener

---
+++ events/ready.js
@@ -0,0 +1,5 @@
+const { client, config }  = require("../index.js");
+
+client.on("ready", () => {
+	console.log(`I, ${client.user.tag}, am at your service. Run ${config.prefix}help to view a list of commands.`);
+});
