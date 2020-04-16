2cb1de0930366f853655d1c1fa790de924952797
unknown <officialspyme@gmail.com>
2020-04-16 23:43:50 +0400

    Handle list too long

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -3,14 +3,21 @@ const { music_handler } = require("../../index.js");
 
 module.exports.execute = async (client, message, args) => {
 
-	let queue = music_handler.getGuildQueue(message.guild.id);
+	const queue = music_handler.getGuildQueue(message.guild.id);
 	if (!queue) return message.reply("⚠️ There is currently no music playing!");
 
+	const queue_trimmed = queue.videos.slice(0, 5); 
+
+	const title = queue.videos.length > 1 ? `Upcoming - Next ${queue.videos.length}` : "Currently Playing"
+
 	let embed = new discord.MessageEmbed()
+		.setTitle(title)
 		.setColor('RANDOM')
 		.setThumbnail(queue.videos[0]["thumbnail"])
-		.setDescription(`**-=- Music Queue -=-**\n${queue.videos.map(video => 
-			`**-** ${video["title"]}`).join('\n')}\n\n🎵 **Currently Playing:** ${queue.videos[0]["title"]}`);
+		.setDescription(`${queue.videos.length > 5 ? "**NOTICE**: This queue only shows the next 5 songs in the queue where there are more than 5 to reaching avoid embed character limit and not showing the embed at all!\n\n" : ""}${queue_trimmed.map((video, index) => 
+			`**${index+1} - ** [${video["title"]}](${video["url"]})`).join('\n')}\n\n🎵 **Currently Playing:** [${queue_trimmed[0]["title"]}](${queue_trimmed[0]["url"]})`)
+		.setFooter(`Requested by ${message.author.tag}`)
+		.setTimestamp();
 
 	message.channel.send(embed);
 
