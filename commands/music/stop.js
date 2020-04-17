73a0396fcc4be18e48c53cfb5b6b58e6326e66c3
unknown <officialspyme@gmail.com>
2020-04-17 22:35:31 +0400

    Add stop command

---
+++ commands/music/stop.js
@@ -0,0 +1,16 @@
+const { music_handler } = require("../../index.js");
+
+module.exports.execute = async (client, message, args) => {
+
+    const queue = music_handler.getGuildQueue(message.guild.id); // Get the queue for the guild the cmd was executed in
+    if (!queue) return message.reply("⚠️ There is currently no music playing!"); // Tell the user no song is being played
+
+    queue.videos = []; // Remove all songs in the queue so nothing to play after the current song ends
+    queue.connection.dispatcher.end(); // End the current song
+
+}
+
+module.exports.help = {
+    name: "stop",
+    aliases: ["end", "leave"]
+}
