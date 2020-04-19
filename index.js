460b45f873af0d944a8c78483e675adb48c679c6
unknown <officialspyme@gmail.com>
2020-04-19 16:15:11 +0400

    Replace project folder in constructor

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const utils = require("./utils/util.js");
 const mh = require("./utils/music_handler.js");
 
 const client = new discord.Client();
-const util = new utils.Utils(client);
+const util = new utils.Utils(client, process.cwd());
 const music_handler = new mh.MusicHandler();
 module.exports = { client, config, util, music_handler };
 
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
 
-util.loadModules(process.cwd() + "\\events\\");
-util.loadModules(process.cwd() + "\\commands\\", true);
+util.loadModules("\\events\\");
+util.loadModules("\\commands\\", true);
 
 
 client.login(config.token);
