9a504967f90d7844ba643801a99e673b02bea5ef
unknown <officialspyme@gmail.com>
2020-04-19 20:23:30 +0400

    Move formatSeconds and replaceStrChar to Utils

--- utils/util.js
+++ utils/util.js
@@ -123,4 +123,12 @@ module.exports.Utils = class Utils {
 
 	}
 
+	formatSeconds = (seconds) => {
+		return new Date(seconds * 1000).toISOString().substr(11, 8)
+	}
+
+	replaceStrChar = (str, index, replacement) => {
+		return str.substr(0, index) + replacement + str.substr(index + replacement.length);
+	}
+
 }
