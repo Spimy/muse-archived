c17f9fd63a2c51ee4286655c414509c9b211bcb2
unknown <officialspyme@gmail.com>
2020-04-19 16:13:58 +0400

    Add unload command

---
+++ commands/admin/unload.js
@@ -0,0 +1,33 @@
+const { config, util } = require("../../index.js");
+
+module.exports.execute = async (client, message, args) => {
+
+    if (!args[0]) return message.reply("⚠️ Specify a command to unload!");
+    const cmd = args[0].toLowerCase();
+    const res = util.unloadCOmmand(cmd, false);
+
+    switch (res) {
+        case "Command Unloaded": {
+            message.reply(`✅ Command \`${cmd}\` unloaded successfully!`);
+            break;
+        }
+        case "Command Not Loaded": {
+            message.reply(`⚠️ Command \`${cmd}\` was never loaded! Use \`${config.prefix}load ${cmd}\` to load it!`);
+            break;
+        }
+        case "Unknown Command": {
+            message.reply("⚠️ The command provided does not exist!");
+            break;
+        }
+        case "Error": {
+            message.reply("⚠️ An unknown an error has occurred when reloding the command!");
+            break;
+        }
+    }
+
+}
+
+module.exports.help = {
+    name: "unload",
+    aliases: ["unloadcmd"]
+}
