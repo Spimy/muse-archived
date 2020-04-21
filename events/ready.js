d3d742269eec5bb7b1b7572f3fddac2d9abee40c
unknown <officialspyme@gmail.com>
2020-04-21 15:14:38 +0400

    Set client activity

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,9 @@
 const { client, config }  = require("../index.js");
 
 client.on("ready", () => {
+
 	console.log(`I, ${client.user.tag}, am at your service. Run ${config.prefix}help to view a list of commands.`);
+
+	client.user.setActivity("commands", { type: "LISTENING"}).catch(console.error);
+
 });
